0f1856bd0dd67c6e9df5c3937e73238642a3ed95
ToXiCCuss <rjstdev@outlook.de>
2025-10-19 01:13:15 +0200

    init

--- src/main/java/de/rjst/ks/OrderController.java
+++ src/main/java/de/rjst/ks/OrderController.java
@@ -4,6 +4,7 @@ import de.rjst.ks.kafka.KafkaWriter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,8 +15,10 @@ public class OrderController {
     private final KafkaWriter kafkaWriter;
 
     @PostMapping
-    public void createOrder() {
-        kafkaWriter.run();
+    public void createOrder(@RequestParam Long count) {
+        for (var i = 0; i < count; i++) {
+            kafkaWriter.run();
+        }
     }
 
 }
--- src/test/java/de/rjst/ks/TestcontainersConfiguration.java
+++ src/test/java/de/rjst/ks/TestcontainersConfiguration.java
@@ -1,5 +1,6 @@
 package de.rjst.ks;
 
+import java.util.List;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,10 @@ class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     public KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+        final var kafkaContainer = new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+        kafkaContainer.setPortBindings(List.of("9092:9092"));
+        return kafkaContainer;
     }
+
 
 }
